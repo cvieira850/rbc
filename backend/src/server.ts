0930aceee244ebc24a3d8f1880ddb6a071d00447
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-05-16 16:08:23 -0300

    Initial Commit

---
+++ backend/src/server.ts
@@ -0,0 +1,32 @@
+import 'reflect-metadata';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
+
+import routes from './routes';
+
+import './database/index';
+
+const app = express();
+
+
+app.use(express.json());
+
+app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  
+
+  // eslint-disable-next-line no-console
+  console.log(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+app.listen(3333, () => {
+  // eslint-disable-next-line no-console
+  console.log('🚀 Server started on port 3333');
+});
