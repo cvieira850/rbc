76b2084ff2073d4df94e19f7b1e9795e7dfe74fa
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-05-17 14:52:19 -0300

    Frontend iniciado

---
+++ frontend/src/App.tsx
@@ -0,0 +1,27 @@
+import React, {useCallback, useState} from 'react';
+
+import './App.css';
+import api from './services/api';
+
+function App() {
+  const [data,setData] = useState({})
+  const getData = useCallback(async () => {
+    const response = await api.get('cases');
+    console.log(response.data);
+
+    setData(response.data);
+  }, []);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <form onSubmit={getData}>
+
+        <button type="submit">Testar</button>
+        </form>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
